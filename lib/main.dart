20f5790e91b2671f05c1998f21e5d51f1c692138
Rituraj Singh <rituraj.singh007@gmail.com>
2023-06-25 20:05:11 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/rituraj.jpg'),
+                radius: 50,
+              ),
+              Text(
+                  'Rituraj Singh',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+                Text(
+                  'QA Engineer',
+                  style: TextStyle(
+                      fontSize: 10,
+                      fontFamily: 'MochiyPopPOne',
+                      color: Colors.white
+                  )
+                )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
